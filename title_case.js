7e58d1b1c92e989b11bece1f617ef3cf115ea960
Katrina <blueyed_qt@hotmail.com>
2017-04-20 12:29:39 -0700

    Working solution that passes all required tests

--- title_case.js
+++ title_case.js
@@ -3,6 +3,20 @@
 // Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
 // For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 
+function titleCase(str) {
+  var newStr = str[0].toUpperCase();
+  for(var i=1; i<str.length; i++) {
+    if(str[i] == " " ) {
+      newStr += " " + str[i+1].toUpperCase();
+      i++;
+    } else {
+      newStr += str[i].toLowerCase();
+    }
+  }
+  return newStr;
+}
+
+titleCase("I'm a little tea pot");
 
 
 // Tests
